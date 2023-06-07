347730d64485de34b74bf2a07441c574cb89e3bb
BABABUSOLA <queenbusola1@gmail.com>
2023-06-07 00:39:24 +0000

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for decimal in range(0,99):
+for decimal in range(0, 99):
     hexadecimal = hex(decimal)
     message = "{} = {}"
     print(message.format(decimal, hexadecimal))
