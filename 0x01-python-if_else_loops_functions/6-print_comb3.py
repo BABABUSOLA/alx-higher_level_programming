96e6cde7133d4a3f1e6a18c70ad06dd996b959d3
BABABUSOLA <queenbusola1@gmail.com>
2023-06-07 01:41:32 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+last_i = None
+last_j = None
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:01d}{:0d}".format(i, j), end=(', ')) 
+        last_i = i
+        last_j = j
+        if i == last_i and j == last_j-1:
+            print("{:01d}{:01d}".format(i, j), end= (', '))
+        else:
+            print("{:01d}{:01d}".format(i, j), end=(' '))
