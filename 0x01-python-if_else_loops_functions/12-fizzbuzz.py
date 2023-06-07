c98532f8afd5fb4bb2fd4da4fa2f6a633e9e8da6
BABABUSOLA <queenbusola1@gmail.com>
2023-06-07 21:12:14 +0000

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@ def fizzbuzz():
     value = None
     for i in range(1, 101):
         if(i % 3 == 0 and i % 5 == 0):
-           value  = "FizzBuzz"
+            value = "FizzBuzz"
         elif(i % 5 == 0):
-           value = "Buzz"
+            value = "Buzz"
         elif(i % 3 == 0):
-           value  = "Fizz"
+            value = "Fizz"
         else:
-           value = i
+            value = i
         print(value, end=(' '))
