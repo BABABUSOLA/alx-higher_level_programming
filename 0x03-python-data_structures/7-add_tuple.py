751490e32331893fce9d9239fa76367ac26909e0
BABABUSOLA <queenbusola1@gmail.com>
2023-06-12 15:50:26 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Print functions that adds 2 turples"""
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    turple_a += (0, 0)
+    turple_b += (0, 0)
+    return (turple_a[0] + turple_b[0], turple_a[1] + turple_b[1])
