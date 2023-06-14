fa30355cde24284db8ebc6f45b01f92431728739
BABABUSOLA <queenbusola1@gmail.com>
2023-06-14 16:58:40 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-"""P
+"""REturns a key with the biggest integer value"""
+
+
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
