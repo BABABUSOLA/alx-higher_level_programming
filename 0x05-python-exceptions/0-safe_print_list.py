8c9b5cb15154e20b7ba6319501126765aec82e9e
BABABUSOLA <queenbusola1@gmail.com>
2023-06-26 13:34:05 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,7 +12,7 @@
 
 def safe_print_list(my_list=[], x=0):
 
-    count = 0;
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i], end="")
@@ -21,4 +21,3 @@ def safe_print_list(my_list=[], x=0):
              break
     print ("")
     return count
-
